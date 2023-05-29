9c863d037039bd55e514e08f56c1b7f7991aa3c7
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-05-29 03:40:22 +0000

    No places in home screen

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,39 +1,52 @@
 class PostsController < ApplicationController
 
-  def show
-    @post = Post.find_by({"id" => params["id"]})
-    @place = Place.find_by({"id" => @post["place_id"]})
-    # render posts/show view with details about Post
-  end
-
   def new
     @post = Post.new
-    @post["place_id"] = params["place_id"]
-    # render posts/new view with new Post form
+    @post.place_id = params["place_id"]
   end
 
   def create
-    # start with a new Post
-    @post = Post.new
+    if current_user
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
 
-    puts params["post"]
-
-    # assign user-entered form data to Contact's columns
-    @post["title"] = params["post"]["title"]
-    @post["user_id"] = params["post"]["user_id"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["created_at"] = params["post"]["created_at"]
-    @post["updated_at"] = params["post"]["updated_at"]
-
-    # assign relationship between Post and Place
-    @post["place_id"] = params["post"]["place_id"]
-
-    # save Post row
-    @post.save
-
-    # redirect user
-    redirect_to "/places/#{@post["place_id"]}"
+      if @post.save
+        redirect_to "/places/#{params["post"]["place_id"]}"
+      else
+        flash.now["notice"] = "Failed to create the post."
+        render :new
+      end
+    else
+      flash["notice"] = "Please log in to create a post."
+      redirect_to login_path
+    end
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :posted_on, :place_id)
+  end
+
+  def require_login
+    unless current_user
+      flash["notice"] = "Please log in to create a post."
+      redirect_to login_path
+    end
+  end
 end
+
+# def create
+#   if @current_user
+#     @post = Post.new
+#     @post["body"] = params["post"]["body"]
+#     @post["image"] = params["post"]["image"]
+#     @post["user_id"] = @current_user["id"]
+#     @post.save
+#   else
+#     flash["notice"] = "Login first."
+#   end
+#   redirect_to "/posts"
+# end
+
+# end
