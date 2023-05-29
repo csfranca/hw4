944feaa459c4a6601152930e2f5f20f575c680c4
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-05-29 01:25:22 +0000

    Controllers, Views and Routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome!"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Incorrect information. Please try again"
+        redirect_to "/sessions/new"
+      end
+    else
+    flash["notice"] = "Incorrect login information. Please try again"
+    redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Logged out"
+    redirect_to "/sessions/new"
+
   end
 end
   
